bc0c4c69266824bce1c3de5f601e5c73c89fb6a9
sparot50 <sperrotta@rogers.com>
2018-12-13 06:44:17 -0500

    Update HelloServlet.java

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
@@ -9,19 +8,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(
-        name = "MyServlet", 
-        urlPatterns = {"/hello"}
-    )
-public class HelloServlet extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        ServletOutputStream out = resp.getOutputStream();
-        out.write("hello Heroku".getBytes());
-        out.flush();
-        out.close();
-    }
-    
+@WebServlet(name = "MyServlet", urlPatterns = {"/hello"})
+public class HelloServlet extends HttpServlet 
+{
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException 
+  {
+    ServletOutputStream out = resp.getOutputStream();
+    out.write("hello Heroku".getBytes());
+    out.flush(); out.close();
+  }  
 }
