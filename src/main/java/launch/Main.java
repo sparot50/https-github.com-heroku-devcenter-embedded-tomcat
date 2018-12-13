acd1c9f158fb5d956a10258bf23d9c935d3ca8fc
Steve Perrotta <Steve Perrotta>
2018-12-13 10:15:36 -0500

    fix intelliJ

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -1,19 +1,16 @@
 package launch;
 
 import java.io.File;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-
-import org.apache.catalina.WebResourceRoot;
+import java.nio.file.Files;
+import java.net.URISyntaxException;
 import org.apache.catalina.WebResourceSet;
-import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.webresources.StandardRoot;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.EmptyResourceSet;
-import org.apache.catalina.webresources.StandardRoot;
-import org.apache.tomcat.util.scan.Constants;
-import org.apache.tomcat.util.scan.StandardJarScanFilter;
 
 public class Main 
 {
